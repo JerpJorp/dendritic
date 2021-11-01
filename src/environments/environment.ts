e7afc27ff4ef3a143f5e07fe63cb0da668f857bb
Kelley Bellard <kelley.bellard@gmail.com>
2021-11-01 15:16:16 -0600

    initial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'dendritic-kb',
+    appId: '1:899168659633:web:ec655982429797ed835a90',
+    storageBucket: 'dendritic-kb.appspot.com',
+    apiKey: 'AIzaSyA4-th4d5T62fXOhk3jOvnIvB6etMzl5vk',
+    authDomain: 'dendritic-kb.firebaseapp.com',
+    messagingSenderId: '899168659633',
+    measurementId: 'G-0JHNSVW9KH',
+  },
   production: false
 };
 
